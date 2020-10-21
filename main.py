8577cfec86182397b946e59a2c0596ee98c3be07
note <victor.mendes056@gmail.com>
2020-10-21 18:00:39 -0300

    asd

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
+import PIL
 from PIL import Image
+import numpy as np
+import pandas as pd
 
 name = "binary.png"
 img = Image.open("imagens/"+name)
+
 
 print("Formato:", img.format)
 print("Size:", img.size)
 print("Mode:", img.mode)
 
-img.show()
+teste = np.array(img)
+print(teste)
+
+df = pd.DataFrame(teste)
+print(df)
+
+#img.show()
