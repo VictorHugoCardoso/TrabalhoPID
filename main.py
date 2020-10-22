3cbff78ad4aa682ca00551f7795b91523bcae1e7
note <victor.mendes056@gmail.com>
2020-10-21 22:15:59 -0300

    finalizado

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import PIL
 from PIL import Image
 import numpy as np
 
-name = "example.png"
+name = "example2.png"
 img = Image.open("imagens/"+name).convert('L').point(lambda x: 0 if x<128 else 255, 'L')
 
 print("Formato:", img.format)
@@ -10,14 +10,31 @@ print("Size:", img.size)
 print("Mode:", img.mode)
 
 ar = np.array(img)
-print(ar)
+print('Antes:\n',ar)
 
-for i in range(ar.shape[0]):
-    for j in range(ar.shape[1]):
-        if(ar[i, j] == 255):
-            ar[i, j] = 0;
-        else:
-            ar[i, j] = 255;
+def dilate(ar):
+    for i in range(ar.shape[0]):
+        for j in range(ar.shape[1]):
+            if (ar[i, j] == 255):
+                if ((i>0) and (ar[i-1, j]==0)):
+                    ar[i-1,j] = 2
+                
+                if ((j>0) and (ar[i, j-1]==0)):
+                    ar[i,j-1] = 2
+                
+                if ((i+1<ar.shape[0]) and (ar[i+1, j]==0)):
+                    ar[i+1, j] = 2
+
+                if ((j+1<ar.shape[1]) and (ar[i, j+1]==0)):
+                    ar[i, j+1] = 2
+
+    for i in range(ar.shape[0]):
+        for j in range(ar.shape[1]):
+            if (ar[i, j] == 2):
+                ar[i, j] = 255
+
+dilate(ar)
+print('Depois:\n',ar)
 
 finalImage = Image.fromarray(np.uint8(ar))
 finalImage.save('imagens/result.png')
