571714ceedfb04c38869dfafb9eea2c05524b973
tiagoviniciuslopes <tiago.pereira@eits.com.br>
2020-11-03 19:41:08 -0300

    ajustes finais pra entrega

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-TrabalhoPID
+Intruções
+
+1. Coloque todas as imagens necessarias na pasta 'imagens' (algumas imagens de teste já estão lá)
+2. Execute o arquivo main.py com o comando: python main.py, uma aba do navegador será aberta
+3. Insira o nome da imagem no campo "Nome da imagem" (a imagem deve estar na pasta 'imagens')
+4. Selecione quais algoritmos serão executados marcando o checkbox de cada um
+5. Insira a quantidade de vezes que o algoritmo será executa no campo "Repeticoes" ao lado do checkbox do algoritmo
+6. Clique na opcao enviar e aguarde o programa finalizar a execucao, as imagens serão geradas dentro da pasta 'imagens/geradas/'
--- main.py
+++ main.py
@@ -158,26 +158,28 @@ def ac_submit(dom):
   dom.focus("input")
 
   caminho_img = dom.get_value("input")
-  img = Image.open(caminho_img).convert('L').point(lambda x: 0 if x < 128 else 255, 'L')
+  img = Image.open('imagens/' + caminho_img).convert('L').point(lambda x: 0 if x < 128 else 255, 'L')
 
   print("Formato:", img.format)
   print("Size:", img.size)
   print("Mode:", img.mode)
 
   array1 = np.array(img)
-  array2 = np.array(img)
+  array2 = array1.copy()
+  array3 = array1.copy()
+  array4 = array1.copy()
 
   if dom.get_value("dilateCross") == "true" and int(dom.get_value("quantityDC")) > 0:
       dilateCross(array1, int(dom.get_value("quantityDC")))  # n
 
   if dom.get_value("dilateSquare") == "true" and int(dom.get_value("quantityDS")) > 0:
-      dilateSquare(array1, int(dom.get_value("quantityDS")))  # n
+      dilateSquare(array2, int(dom.get_value("quantityDS")))  # n
 
   if dom.get_value("erodeCross") == "true" and int(dom.get_value("quantityEC")) > 0:
-      erodeCross(array2, int(dom.get_value("quantityEC")))
+      erodeCross(array3, int(dom.get_value("quantityEC")))
 
   if dom.get_value("erodeSquare") == "true" and int(dom.get_value("quantityES")) > 0:
-      erodeSquare(array2, int(dom.get_value("quantityES")))
+      erodeSquare(array4, int(dom.get_value("quantityES")))
 
   dom.alert("Imagens geradas e salvas na pasta \"imagens\geradas\" uma pasta dentro da pasta dos trabalho")
 
